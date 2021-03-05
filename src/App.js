f4f190105013f56c3e5d2187a461bf2a42c4e62b
bagpyp <robert@bagpyp.net>
2021-03-04 18:51:31 -0800

    add pick toggle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 
-const ENV = 'prod'
+const ENV = 'PROD'
 
 function App() {
 
-  const PICKLIST_URL = ((ENV === 'DEV') ? 'http://localhost:5000' : 'https://hillcrestsports-picklist.herokuapp.com')
+  const PICKLIST_URL = ((ENV === 'DEV') ? 'http://localhost:5000/' : 'https://hillcrestsports-picklist.herokuapp.com/')
   const [date,setDate] = useState('')
 
   function Picklist() {
+    const [a,setA] = useState(0)
     const [picks,setPicks] = useState([])
     const getPicks = async () => {
       const res = await fetch(PICKLIST_URL)
@@ -18,18 +19,29 @@ function App() {
     }
     useEffect(() => {
       getPicks()
-    }, [])
+    }, [a])
 
+    function Pick(x) {
 
-    return (
-      <div className="picklist">
-        {picks.map((x,index) => (
-        <div className = "pick" key={index}>
+      // const [picked, setPicked] = useState(x['picked'])
+      const id = x['app_id']
+      var picked = x['picked']
+
+      const togglePicked = async () => {
+        const res = await fetch(PICKLIST_URL+'pick?app_id='+id, {method:'PUT'})
+        const json_res = await res.json()
+        picked = json_res
+        setA(a+1)
+      }
+
+      return (
+        <div className = {(picked || x['picked']) ? 'pick picked' : 'pick'} key={id}>
 
           <img src={x['v_image_url']} alt="product"></img>
           <div className = "sku">{x["sku"]}</div>
           <div className = "qty">{x["qty"]}</div>
           <div className = "created_date">{new Date(x["created_date"]).toLocaleDateString('en-US')}</div>
+          
           <div className = "data">
             <div className = "tag">{x["tag"] + (x["app_num_other_items"] ? (' 1/'+ (parseInt(x["app_num_other_items"]) + 1).toString()) : '')}</div>
             <div className = "app_name">{x["app_name"]}</div>
@@ -42,10 +54,21 @@ function App() {
           </div>  
 
           <div className = "buttons">
-          {/* <Button variant="primary">Primary</Button>{' '} */}
+            <label className="switch">
+              {picked ? 
+                <input type="checkbox" onChange={togglePicked} checked></input> 
+                : <input type="checkbox" onChange={togglePicked}></input>}
+              <span className="slider round"></span>
+            </label>
           </div>
+
         </div>
-        ))}
+      )
+    }
+
+    return (
+      <div className="picklist">
+        {picks.map(x => Pick(x))}
       </div>
     )
   }
